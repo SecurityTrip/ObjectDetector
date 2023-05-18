e2ce04c87b06077167d6b85f45df7a5a13c406af
ilia <illigiorno@gmail.com>
2023-05-18 11:39:43 +0400

    Added Camera vision and FPS counter

--- main.py
+++ main.py
@@ -24,8 +24,70 @@ import time
 
 import cv2
 
-# Print Tensorflow version
-print(tf.__version__)
+def returnCameraIndexes():
+    # checks the first 10 indexes.
+    index = 0
+    arr = []
+    i = 10
+    while i > 0:
+        cap = cv2.VideoCapture(index)
+        if cap.read()[0]:
+            arr.append(index)
+            cap.release()
+        index += 1
+        i -= 1
+    return arr
 
-# Check available GPU devices.
-print("The following GPU devices are available: %s" % tf.test.gpu_device_name())
+print(returnCameraIndexes())
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+# used to record the time when we processed last frame
+prev_frame_time = 0
+
+# used to record the time at which we processed current frame
+new_frame_time = 0
+
+while (True):
+
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+
+    # font which we will be using to display FPS
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # time when we finish processing for this frame
+    new_frame_time = time.time()
+
+    # Calculating the fps
+
+    # fps will be number of frame processed in given time frame
+    # since their will be most of time error of 0.001 second
+    # we will be subtracting it to get more accurate result
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+
+    # converting the fps into integer
+    fps = int(fps)
+
+    # converting the fps to string so that we can display it on frame
+    # by using putText function
+    fps = str(fps)
+
+    # putting the FPS count on the frame
+    cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+
+    # displaying the frame with fps
+    cv2.imshow('frame', frame)
+
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
